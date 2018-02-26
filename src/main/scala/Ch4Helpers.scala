de3b843e3e9c1a393b407556f3cf03944ba85fc7
Gregory Chen <gchen213@bloomberg.net>
2018-02-26 14:22:09 -0500

    Add Ch4 Ex2 solution

--- src/main/scala/Ch4Helpers.scala
+++ src/main/scala/Ch4Helpers.scala
@@ -3,4 +3,11 @@ object Ch4Helpers {
     val DISCOUNT_RATE = 0.1
     for ((k, v) <- inMap) yield (k, v - v * DISCOUNT_RATE)
   }
+
+  def getWordsAndCounts(inStr: String) = {
+    val in = new java.util.Scanner(inStr)
+    val wordCounts = collection.mutable.Map[String, Int]().withDefaultValue(0);
+    while (in.hasNext) wordCounts(in.next) += 1
+    wordCounts
+  }
 }
--- src/test/scala/Ch4Tests.scala
+++ src/test/scala/Ch4Tests.scala
@@ -10,3 +10,49 @@ class Ch4Ex1Test extends FunSuite {
     }
   }
 }
+
+class Ch4Ex2Test extends FunSuite {
+  test("Empty string should have no word counts") {
+    val wordCounts = Ch4Helpers.getWordsAndCounts("")
+    assert(wordCounts.size == 0)
+  }
+
+  test("The String [Hello] should have correct word counts") {
+    val testStr = "Hello"
+    val wordCounts = Ch4Helpers.getWordsAndCounts(testStr)
+    assert(wordCounts.size == 1)
+    assert(wordCounts("Hello") == 1)
+  }
+
+  test("The String [Hello Hello] should have correct word counts") {
+    val testStr = "Hello Hello"
+    val wordCounts = Ch4Helpers.getWordsAndCounts(testStr)
+    assert(wordCounts.size == 1)
+    assert(wordCounts("Hello") == 2)
+  }
+
+  test("The String [Hello World] should have correct word counts") {
+    val testStr = "Hello World"
+    val wordCounts = Ch4Helpers.getWordsAndCounts(testStr)
+    assert(wordCounts.size == 2)
+    assert(wordCounts("Hello") == 1)
+    assert(wordCounts("World") == 1)
+  }
+
+  test("The String [Hello Hello World] should have correct word counts") {
+    val testStr = "Hello Hello World"
+    val wordCounts = Ch4Helpers.getWordsAndCounts(testStr)
+    assert(wordCounts.size == 2)
+    assert(wordCounts("Hello") == 2)
+    assert(wordCounts("World") == 1)
+  }
+
+
+  test("The String [Hello World Hello] should have correct word counts") {
+    val testStr = "Hello World Hello"
+    val wordCounts = Ch4Helpers.getWordsAndCounts(testStr)
+    assert(wordCounts.size == 2)
+    assert(wordCounts("Hello") == 2)
+    assert(wordCounts("World") == 1)
+  }
+}
