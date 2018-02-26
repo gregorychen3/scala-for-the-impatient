534757670d8bb82cbc078dacc5e587d993ffa3fe
Gregory Chen <gchen213@bloomberg.net>
2018-02-26 17:53:08 -0500

    Refactor Ch5Ex1 solution

--- src/main/scala/Ch5Helpers.scala
+++ src/main/scala/Ch5Helpers.scala
@@ -1,9 +1,7 @@
-object Ch5Helpers {
-  class Counter {
-    private var value = 0
-    def increment(): Unit = {
-      if (value < Int.MaxValue) value += 1
-    }
-    def current() = value
+class Counter {
+  private var value = 0
+  def increment(): Unit = {
+    if (value < Int.MaxValue) value += 1
   }
+  def current() = value
 }
