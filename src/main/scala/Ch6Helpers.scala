62864f1919baa9b164f796465c4d21b9b61e8c8c
Gregory Chen <gchen213@bloomberg.net>
2018-02-27 18:00:47 -0500

    Remove accidentally committed code

--- src/main/scala/Ch6Helpers.scala
+++ src/main/scala/Ch6Helpers.scala
@@ -19,7 +19,3 @@ class Point(val x: Double, val y: Double) {}
 object Point {
   def apply(x: Double, y: Double) = new Point(x, y)
 }
-
-object Ch6Helpers {
-  def concatReverse(a: Array[String]): String = a.fol
-}
