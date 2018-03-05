fb6adaf1bea26d2d37cd243c2c8d7c3913d3c137
Gregory Chen <gchen213@bloomberg.net>
2018-03-05 10:38:47 -0500

    Add Ch8 Ex1 tests

--- src/test/scala/Ch8Tests.scala
+++ src/test/scala/Ch8Tests.scala
@@ -6,4 +6,24 @@ class Ch8Ex1Test extends FunSuite {
     val ba = new Ch8.BankAccount(initialBalance)
     assert(ba.currentBalance == initialBalance)
   }
+
+  test("Withdrawing from BankAccount should change balance") {
+    val initialBalance = 100
+    val ba = new Ch8.BankAccount(initialBalance)
+    assert(ba.currentBalance == initialBalance)
+
+    val withdrawAmount = 1
+    ba.withdraw(withdrawAmount)
+    assert(ba.currentBalance == initialBalance - withdrawAmount)
+  }
+
+  test("Depositing to BankAccount should change balance") {
+    val initialBalance = 100
+    val ba = new Ch8.BankAccount(initialBalance)
+    assert(ba.currentBalance == initialBalance)
+
+    val depositAmount = 1
+    ba.deposit(depositAmount)
+    assert(ba.currentBalance == initialBalance + depositAmount)
+  }
 }
